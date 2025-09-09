57193a70a1bb99d3782e3e727aa9332cf7cae48e
Your Name <your.email@example.com>
2025-09-10 00:25:38 +0900

    Fix configuration files and update project structure

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname, './'),
-};
-
-module.exports = nextConfig;
